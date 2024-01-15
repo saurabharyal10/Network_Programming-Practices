8e088ddf40b5e3100f8a92a8216d4ad32fd5b3b1
Saurabh Aryal <saurabharyal10@gmail.com>
2024-01-15 22:35:50 +0545

    UDP commit

---
+++ UDPClient.java
@@ -0,0 +1,12 @@
+package udp;
+
+import java.net.DatagramSocket;
+
+public class UDPClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DatagramSocket 
+	}
+
+}
